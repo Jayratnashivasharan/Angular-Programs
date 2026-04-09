8ebf736964aa944b0cad8ee8f2ae2e1ec5d68e0b
Jayratna <jayratnashivasharan8@gmail.com>
2026-04-09 09:08:31 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtondemoComponent } from './buttondemo/buttondemo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtondemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/buttondemo/buttondemo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttondemo',
+  imports: [],
+  templateUrl: './buttondemo.component.html',
+  styleUrl: './buttondemo.component.css'
+})
+export class ButtondemoComponent {
+  name : String ='Honey';
+}
