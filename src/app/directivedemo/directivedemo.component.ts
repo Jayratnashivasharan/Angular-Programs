0f96ee0994857706fcaa05f272b660e90075d484
Jayratna <jayratnashivasharan8@gmail.com>
2026-04-10 09:51:06 +0530

    done

--- src/app/directivedemo/directivedemo.component.ts
+++ src/app/directivedemo/directivedemo.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class DirectivedemoComponent {
   isLogin=true;
 
-  cities=["pune","Mumbai","solapur"];
+  cities=["Pune","Mumbai","Solapur"];
 
   pcolor="red";
 }
